5ea029d31b9675e42261f32357dc5b2ba677e866
BCRSY <36918910+BCRSY@users.noreply.github.com>
2021-11-26 15:29:21 +0700

    documentation skeleton

--- documentations/website/docs/API/avatar.md
+++ documentations/website/docs/API/avatar.md
@@ -2,7 +2,7 @@
 sidebar_position: 1
 ---
 
-# Create a Page
+# Avatar
 
 Add **Markdown or React** files to `src/pages` to create a **standalone page**:
 
--- documentations/website/src/components/HomepageFeatures.js
+++ documentations/website/src/components/HomepageFeatures.js
@@ -4,32 +4,29 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Fitur1',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Berisikan fitur-fitur VIvelJS yang menarik.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Fitur2',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Berisikan fitur-fitur VIvelJS yang menarik.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Fitur3',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Berisikan fitur-fitur VIvelJS yang menarik.
       </>
     ),
   },
