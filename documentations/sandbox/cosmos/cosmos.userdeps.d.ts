b03c53906ccbd432985ecf616f391a977b891496
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2021-12-22 20:12:36 +0700

    remove react cosmos

--- documentations/sandbox/cosmos/cosmos.userdeps.d.ts
+++
@@ -1,9 +0,0 @@
-import {
-  ReactDecoratorsByPath,
-  ReactFixtureExportsByPath,
-} from 'react-cosmos-shared2/react';
-import { DomRendererConfig } from 'react-cosmos/dist/shared/rendererConfig';
-
-export const rendererConfig: DomRendererConfig;
-export const fixtures: ReactFixtureExportsByPath;
-export const decorators: ReactDecoratorsByPath;
--- documentations/sandbox/cosmos/index.tsx
+++
@@ -1,6 +0,0 @@
-import { mountDomRenderer } from 'react-cosmos/dom';
-import { decorators, fixtures, rendererConfig } from './cosmos.userdeps';
-
-mountDomRenderer({ rendererConfig, decorators, fixtures });
-
-if (import.meta.hot) import.meta.hot!.accept();
